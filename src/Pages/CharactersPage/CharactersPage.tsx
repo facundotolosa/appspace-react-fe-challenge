915469e27fe6610af14d32906fdbda321fc3b359
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-04 13:59:35 -0300

    Functionality for pagination added to the footer

--- src/Pages/CharactersPage/CharactersPage.tsx
+++ src/Pages/CharactersPage/CharactersPage.tsx
@@ -7,20 +7,50 @@ import Footer from '../../components/Footer/Footer';
 
 const CharactersPage = () => {
 	const [charactersToShow, setCharactersToShow] = useState([]);
+	const [nextPage, setNextPage] = useState('');
+	const [previousPage, setPreviousPage] = useState('');
 
 	useEffect(() => {
 		(async () => {
 			const {
-				data: { results }
+				data: { results, info }
 			} = await axios.get(`https://rickandmortyapi.com/api/character`);
 			setCharactersToShow(results);
+			setNextPage(info.next);
 		})();
 	}, []);
+
+	const showNextPage = () => {
+		(async () => {
+			window.scrollTo(0, 0);
+			const {
+				data: { results, info }
+			} = await axios.get(nextPage);
+
+			setCharactersToShow(results);
+			setPreviousPage(info.prev);
+			setNextPage(info.next);
+		})();
+	};
+
+	const showPreviousPage = () => {
+		(async () => {
+			window.scrollTo(0, 0);
+			const {
+				data: { results, info }
+			} = await axios.get(previousPage);
+
+			setCharactersToShow(results);
+			setPreviousPage(info.prev);
+			setNextPage(info.next);
+		})();
+	};
 
 	return (
 		<CharactersPageStyled>
 			<Header />
 			<CharacterList charactersToShow={charactersToShow} />
+			<Footer actualPage={1} nextPage={showNextPage} previousPage={showPreviousPage} />
 		</CharactersPageStyled>
 	);
 };
