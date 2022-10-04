aeb7b06c73c646a67fc6f131512f024325eff6ad
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-04 10:51:28 -0300

    Character component tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import Character from './components/Character/Character';
+import { character } from './mocks/characterMocks';
 import styledMainTheme from './styledMainTheme';
 
 const App = () => {
-	const character = {
-		name: 'Rick Sanchez',
-		image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-		species: 'Human',
-		gender: 'Male'
-	};
-
 	return (
 		<ThemeProvider theme={styledMainTheme}>
 			<h1>Website under construction</h1>
