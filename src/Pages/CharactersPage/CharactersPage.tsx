93916303b275718d9e3c70c57bf3a105eb7346b5
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-04 11:55:21 -0300

    Fetch made to the api at CharactersPage

--- src/Pages/CharactersPage/CharactersPage.tsx
+++ src/Pages/CharactersPage/CharactersPage.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import CharacterList from '../../components/CharacterList/CharacterList';
-import { charactersToShow } from '../../mocks/characterMocks';
+import axios from 'axios';
 
 const CharactersPage = () => {
+	const [charactersToShow, setCharactersToShow] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const {
+				data: { results }
+			} = await axios.get(`https://rickandmortyapi.com/api/character`);
+			setCharactersToShow(results);
+		})();
+	}, []);
+
 	return (
 		<>
 			<h1>Website under construction</h1>
