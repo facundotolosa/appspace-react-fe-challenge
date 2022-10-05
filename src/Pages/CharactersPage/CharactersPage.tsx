e055d5f5f71eb0af803afa2a50fcdd18b7914030
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-05 11:53:43 -0300

    Pagination functions refactored to one function

--- src/Pages/CharactersPage/CharactersPage.tsx
+++ src/Pages/CharactersPage/CharactersPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import CharacterList from '../../components/CharacterList/CharacterList';
 import axios from 'axios';
 import Header from '../../components/Header/Header';
@@ -8,39 +8,41 @@ import { useQuery } from 'react-query';
 
 const CharactersPage = () => {
 	const [currentPage, setCurrentPage] = useState(1);
-	let link = 'https://rickandmortyapi.com/api/character/';
+	let requestURL = 'https://rickandmortyapi.com/api/character/';
 
 	const { data, error, isLoading, refetch } = useQuery(['characters'], async () => {
-		const { data } = await axios.get(link);
+		const {
+			data: {
+				info: { next, prev, pages },
+				results
+			}
+		} = await axios.get(requestURL);
 
-		return data;
+		return { next, prev, pages, results };
 	});
 
-	const showNextPage = () => {
-		link = data.info.next;
-		setCurrentPage(currentPage + 1);
-		refetch();
-	};
-
-	const showPreviousPage = () => {
-		link = data.info.prev;
-		setCurrentPage(currentPage - 1);
+	const paginate = (nextPage: boolean) => {
+		requestURL = nextPage ? data?.next : data?.prev;
+		setCurrentPage(nextPage ? currentPage + 1 : currentPage - 1);
+		window.scrollTo(0, 0);
 		refetch();
 	};
 
 	return (
 		<CharactersPageStyled>
 			<Header />
-			{data ? <CharacterList charactersToShow={data?.results} /> : ''}
-			{data ? (
-				<Footer
-					actualPage={currentPage}
-					nextPage={showNextPage}
-					previousPage={showPreviousPage}
-					totalPages={data.info.pages}
-				/>
+			{isLoading ? (
+				<span>Loading...</span>
 			) : (
-				''
+				<>
+					<CharacterList charactersToShow={data?.results} />{' '}
+					<Footer
+						actualPage={currentPage}
+						nextPage={() => paginate(true)}
+						previousPage={() => paginate(false)}
+						totalPages={data?.pages}
+					/>
+				</>
 			)}
 		</CharactersPageStyled>
 	);
