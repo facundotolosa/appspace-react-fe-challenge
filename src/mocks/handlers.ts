ba7360bafad26317849ca85d1665ebdf2fee2e61
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-08 16:06:17 -0300

    Integration test made for DetailPage

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { rest } from 'msw';
-import { apiResponseTwoCharacters } from './characterMocks';
+import { apiResponseTwoCharacters, characterDetail } from './characterMocks';
 
 export const handlers = [
 	rest.get(`${process.env.REACT_APP_API_URL}`, (_req, res, ctx) =>
 		res(ctx.status(200), ctx.json(apiResponseTwoCharacters))
-	)
+	),
+	rest.get(`${process.env.REACT_APP_API_URL}44`, (_req, res, ctx) => res(ctx.status(200), ctx.json(characterDetail)))
 ];
