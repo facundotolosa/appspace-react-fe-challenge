08b09d1377eb1c703fa4374975278d0e6616564c
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-08 15:20:05 -0300

    Back button added to detail page

--- src/Pages/DetailPage/DetailPageStyled.ts
+++ src/Pages/DetailPage/DetailPageStyled.ts
@@ -4,4 +4,28 @@ export const DetailPageStyled = styled.div`
 	background-color: ${props => props.theme.fifthColor};
 	min-height: 100vh;
 	padding-bottom: 10px;
+
+	.back-button {
+		display: flex;
+		justify-content: center;
+
+		button {
+			font-family: 'Roboto', sans-serif;
+			font-size: 16px;
+			color: white;
+			font-weight: 600;
+			background-color: ${props => props.theme.secondColor};
+			box-shadow: 2px 3px 5px 0px rgba(0, 0, 0, 0.75);
+			border: none;
+			width: 150px;
+			height: 50px;
+			transition: all 0.5s;
+
+			&:hover {
+				cursor: pointer;
+				background-color: white;
+				color: ${props => props.theme.secondColor};
+			}
+		}
+	}
 `;
