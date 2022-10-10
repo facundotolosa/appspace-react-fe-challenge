507006ba55905fb8f5676f4e3e04e94b28d99e43
Facundo Tolosa <facundotolosa98@gmail.com>
2022-10-10 19:54:14 -0300

    Added title and name to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,9 @@ const Header = (): JSX.Element => {
 	return (
 		<HeaderStyled>
 			<img src="/images/rick-and-morty-logo.png" alt="Rick and Morty logo" />
+			<div className="title-and-name">
+				<h1 className="title">Appspace FE challenge</h1> <span className="name">Facundo Tolosa</span>
+			</div>
 		</HeaderStyled>
 	);
 };
--- src/components/Header/HeaderStyled.ts
+++ src/components/Header/HeaderStyled.ts
@@ -2,13 +2,49 @@ import styled from 'styled-components';
 
 export const HeaderStyled = styled.header`
 	width: 100%;
-	height: 120px;
+	height: 185px;
 	padding: 5px;
 	background-color: ${props => props.theme.firstColor};
 	display: flex;
+	flex-direction: column;
 	justify-content: center;
+	color: white;
+	font-family: 'Roboto', sans-serif;
+	align-items: center;
+
+	.name {
+		font-style: italic;
+	}
+
+	.title-and-name {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 5px;
+	}
+
+	.title {
+		margin: 0;
+		font-size: 28px;
+	}
 
 	img {
-		height: 100%;
+		height: 110px;
+	}
+
+	@media (min-width: 600px) {
+		height: 120px;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 5px 20px;
+		gap: 10px;
+
+		.title {
+			font-size: 23px;
+		}
+
+		.title-and-name {
+			align-items: flex-start;
+		}
 	}
 `;
